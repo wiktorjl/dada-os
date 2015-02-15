de82f20496ed248b5ac9ee729dc2691d11a3291e
Wiktor Lukasik <wiktor@lukasik.org>
2015-02-15 12:30:35 -0800

    Cleaning up types.h

--- src/types.h
+++ src/types.h
@@ -6,23 +6,24 @@
 #ifndef types_h
 #define types_h
 
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
-typedef uint32_t size_t;
 typedef enum { FALSE=0, TRUE } bool_t;
 
-//typedef char sint8_t;
-//typedef short sint16_t;
-//typedef int sint32_t;
-//typedef long long sint64_t;
-//typedef unsigned long long uint64_t;
-//typedef uint32_t addr_t;
-//typedef uint32_t offset_t;
-//typedef uint64_t loffset_t;
-//typedef uint32_t time_t;
-//typedef sint32_t pid_t;
-//typedef uint8_t  ret_t;
-//typedef uint8_t  byte_t;
+typedef unsigned char       uint8_t;
+typedef unsigned short      uint16_t;
+typedef unsigned int        uint32_t;
+
+typedef char                sint8_t;
+typedef short               sint16_t;
+typedef int                 sint32_t;
+typedef long long           sint64_t;
+typedef unsigned long long  uint64_t;
+typedef uint32_t            addr_t;
+typedef uint32_t            offset_t;
+typedef uint64_t            loffset_t;
+typedef uint32_t            time_t;
+typedef uint32_t            size_t;
+typedef sint32_t            pid_t;
+typedef uint8_t             ret_t;
+typedef uint8_t             byte_t;
 
 #endif
